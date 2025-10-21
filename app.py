2cf12251bc9b76bc6057d7c05488a3439d643d9e
{shivansh} <{goelshivansh8343@gmail.com}>
2025-10-21 19:04:21 +0530

    Added to github

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.datasets import load_iris
+data=load_iris()
+x=data.data
+y=data.target
+df=pd.DataFrame(x)
+df["Target"]=y
+print(df.isnull().sum())
